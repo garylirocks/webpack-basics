06d0386c3db18738f8805479466e6087a55e5b89
Gary Li <gary.li.rocks@gmail.com>
2017-01-30 16:58:12 +1300

    add configs and an example to demo compiling react, sass and images

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: 'build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader',
+            },
+            {
+                test: /\.(png|jpg)/,
+                loader: 'url-loader?limit=200000'
+            }
+        ]
+    }
+}
